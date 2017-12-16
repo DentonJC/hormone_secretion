74757451050df88bc9234c0d828ae26f0dcdf1e9
DentonJC <rigger@tuta.io>
2017-12-16 11:40:27 +0100

    readme

--- hormones.py
+++ hormones.py
@@ -15,7 +15,11 @@ def func2(a, b, k, t, x):
     return a/k - b*(k/(k**2 + (math.pi/12)**2)*math.cos(math.pi*t/12) + math.pi/(12*(k**2 + (math.pi/12)**2))*math.sin(math.pi*t/12)) + math.e**(-k*t)*(x0 - a/k + b*k/(k**2 + (math.pi/12)**2))
 
 
-def Euler(a, b, k, h, x, y, period):
+def Euler(a, b, k, h, y, period):
+    """
+    Forward Euler method
+    """
+    x = 0
     X, Y = [], []
     while x < period:
         y += h * func(x, y, a, b, k)
@@ -25,9 +29,9 @@ def Euler(a, b, k, h, x, y, period):
     return X, Y
 
 
-def Analytic(a, b, k, x0, y, h, period):
+def Analytic(a, b, k, x0, h, period):
+    t, y = 0, 0
     X, Y = [], []
-    t = 0
     while t <= period:
         y = func2(a, b, k, t, x0)
         t = t + h
@@ -36,9 +40,9 @@ def Analytic(a, b, k, x0, y, h, period):
     return X, Y
 
 
-def main(a, b, k, h, x, y, period):
-    X_e, Y_e = Euler(a, b, k, h, y, x, period)
-    X_a, Y_a = Analytic(a, b, k, x, y, h, period)
+def main(a, b, k, h, x, period):
+    X_e, Y_e = Euler(a, b, k, h, x, period)
+    X_a, Y_a = Analytic(a, b, k, x, h, period)
     plt.title("Secretion of hormones " + 'MSE = ' + str(mean_squared_error(Y_e, Y_a)))
     plt.plot(X_e, Y_e, c="green", label="euler")
     plt.plot(X_a, Y_a, c="red", label="analytical")
@@ -52,8 +56,8 @@ def main(a, b, k, h, x, y, period):
 if __name__ == "__main__":
     b = 1
     a = 1
-    h = 0.001
     k = 2
     x0 = 0.1
-    y0 = 0
-    main(a, b, k, h, x0, y0, period=100)
+    period = 100 # length of the time axis
+    h = 0.001 # step
+    main(a, b, k, h, x0, period)
